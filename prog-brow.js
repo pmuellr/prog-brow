9bad083249427cf2f069bf95a73ce2fd45dc8962
Patrick Mueller <pmuellr@gmail.com>
2023-05-19 15:26:41 -0400

    open a fixed file with the text renderer

--- prog-brow.js
+++ prog-brow.js
@@ -5,29 +5,36 @@ const { app, BrowserWindow } = require('electron')
 main()
 
 async function main() {
+  const { start: serverStart } = await import('./lib/server.mjs')
+
+  const port = await serverStart()
+
   app.on('window-all-closed', () => app.quit())
 
   await app.whenReady()
-  appReady()
+  appReady(port)
 }
 
-
-function appReady() {
-  createWindow()
+/** @type { (port: number) => void } */
+function appReady(port) {
+  createWindow(port)
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow(port)
     }
   })
 }
 
-function createWindow() {
+/** @type { (port: number) => BrowserWindow } */
+function createWindow(port) {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     icon: './images/prog-brow.png'
   })
 
-  win.loadFile('./lib/loading.html')
+  win.loadURL(`http://localhost:${port}/render/Users/pmuellr/Projects/pmuellr/prog-brow/prog-brow.js`)
+
+  return win
 }
